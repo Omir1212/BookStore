cb21fcdf1d1ff400d1c478ab3b6a76cb27ef183e
Omir1212 <omir.hosny1212@gmail.com>
2023-09-24 20:58:48 +0300

    Frontend Added

--- Store/urls.py
+++ Store/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , re_path, include
+from django.conf.urls.static import serve
+
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    re_path(r'^(?P<path>.*)$', serve, {'document_root': settings.FRONTEND_ROOT}),
     path('BookStore/', include('BookStore.urls')),
 ]
