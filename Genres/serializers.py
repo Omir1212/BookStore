ff7a32e0fb10af4f3d0529f461834af5d24d32c7
Omir11 <omir.hosny1212@premier.cloudypedia.com>
2023-11-12 21:13:07 +0200

    changes

--- Genres/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from . import models
-
-
-class GenreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Genre
-        fields = '__all__'
